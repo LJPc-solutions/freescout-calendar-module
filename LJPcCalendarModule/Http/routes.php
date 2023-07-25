e7c812ff2b087a9a2637d138dc9d29e969e76bc9
Lars Jansen <lars@ljpc.nl>
2023-07-25 10:08:00 +0200

    fix: secured requests to /calendar

--- LJPcCalendarModule/Http/routes.php
+++ LJPcCalendarModule/Http/routes.php
@@ -1,11 +1,11 @@
 <?php
 
 Route::group( [ 'middleware' => 'web', 'prefix' => Helper::getSubdirectory(), 'namespace' => 'Modules\LJPcCalendarModule\Http\Controllers' ], function () {
-	Route::get( '/calendar', [ 'uses' => 'LJPcCalendarModuleController@index' ] )->name( 'ljpccalendarmodule.index' );
-	Route::get( '/calendar/ajax', [ 'uses' => 'LJPcCalendarModuleController@getItems' ] )->name( 'ljpccalendarmodule.ajax' );
-	Route::post( '/calendar/ajax', [ 'uses' => 'LJPcCalendarModuleController@ajax' ] )->name( 'ljpccalendarmodule.ajax' );
+		Route::get( '/calendar', [ 'uses' => 'LJPcCalendarModuleController@index', 'middleware' => [ 'auth', 'roles' ], 'roles' => [ 'user', 'admin' ] ] )->name( 'ljpccalendarmodule.index' );
+		Route::get( '/calendar/ajax', [ 'uses' => 'LJPcCalendarModuleController@getItems', 'middleware' => [ 'auth', 'roles' ], 'roles' => [ 'user', 'admin' ] ] )->name( 'ljpccalendarmodule.ajax' );
+		Route::post( '/calendar/ajax', [ 'uses' => 'LJPcCalendarModuleController@ajax', 'middleware' => [ 'auth', 'roles' ], 'roles' => [ 'user', 'admin' ] ] )->name( 'ljpccalendarmodule.ajax' );
 } );
 
 Route::group( [ 'prefix' => Helper::getSubdirectory(), 'namespace' => 'Modules\LJPcCalendarModule\Http\Controllers' ], function () {
-	Route::get( '/calendar/{id}', [ 'uses' => 'LJPcCalendarModuleController@export' ] )->name( 'ljpccalendarmodule.external' );
+		Route::get( '/calendar/{id}', [ 'uses' => 'LJPcCalendarModuleController@export' ] )->name( 'ljpccalendarmodule.external' );
 } );
