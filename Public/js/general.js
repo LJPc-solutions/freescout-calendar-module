3c7a9061f667afcef3898bb3b0b83891860ab26a
Lars Jansen <lars@ljpc.nl>
2024-11-08 09:52:50 +0100

    fix: restored the original attachment link

--- Public/js/general.js
+++ Public/js/general.js
@@ -2,18 +2,4 @@ $(document).ready(function () {
     $('.dash-card.ljpc_move_dash_card').each(function (i, el) {
         $('.dash-cards').prepend(el);
     });
-
-    // Check if there is an element with the class "add-attachment-to-calendar"
-    if ($('.add-attachment-to-calendar').length) {
-        // Get the data-attachment-id value from the element with the class "add-attachment-to-calendar"
-        var attachmentId = $('.add-attachment-to-calendar').data('attachment-id');
-
-        // Check if there is another element with the same data-attachment-id but without the "add-attachment-to-calendar" class
-        var elementToRemove = $('[data-attachment-id="' + attachmentId + '"]:not(.add-attachment-to-calendar)');
-
-        // If the element exists, remove it
-        if (elementToRemove.length) {
-            elementToRemove.remove();
-        }
-    }
 });
